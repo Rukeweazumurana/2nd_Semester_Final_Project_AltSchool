d2f4bf9a751e93a3aa37cd78dd574427156d5e04
Rukewe Azumurana <babyrukkie@gmail.com>
2023-10-18 11:02:57 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ErrorBoundary from './Components/ErrorBoundary';
